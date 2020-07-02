75a26a38a8ca50c7e55ef70786c834d4a3b38a8d
Jperzval <perezvalle.jes@gmail.com>
2020-07-02 16:57:29 -0400

    implemented overridden methods on GnG and TestingSite viewHolder and adapter classes. Also created TODOs

--- app/src/main/java/com/nycapp/nycaid/Presenter/Food/RCV/GrabNGoViewHolder.java
+++ app/src/main/java/com/nycapp/nycaid/Presenter/Food/RCV/GrabNGoViewHolder.java
@@ -1,4 +1,19 @@
 package com.nycapp.nycaid.Presenter.Food.RCV;
 
-public class GrabNGoViewHolder {
+import android.app.Activity;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GrabNGoViewHolder extends RecyclerView.ViewHolder {
+
+    public GrabNGoViewHolder(@NonNull View itemView) {
+        super(itemView);
+        //TODO: Bind views
+    }
+
+    public void onBind(){
+        //TODO: Set views
+    }
 }
--- app/src/main/java/com/nycapp/nycaid/Presenter/Health/RCV/TestSiteAdapter.java
+++ app/src/main/java/com/nycapp/nycaid/Presenter/Health/RCV/TestSiteAdapter.java
@@ -1,4 +1,35 @@
 package com.nycapp.nycaid.Presenter.Health.RCV;
 
-public class TestSiteAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nycapp.nycaid.R;
+
+public class TestSiteAdapter extends RecyclerView.Adapter<TestSiteViewHolder> {
+
+    //TODO: Created private variable for model class list
+    //TODO: Create constructor with model class list parameter
+
+    @NonNull
+    @Override
+    public TestSiteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.itemview_test_site, parent, false);
+        return new TestSiteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TestSiteViewHolder holder, int position) {
+        holder.onBind();//modelClassList.get(position)
+    }
+
+    @Override
+    public int getItemCount() {
+        //TODO: Return modelClassList.size()
+        return 0;
+    }
 }
