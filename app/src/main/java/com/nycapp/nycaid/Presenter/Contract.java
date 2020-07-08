5d336533eca5ab74948d3d90172d2cde226c66aa
Jperzval <perezvalle.jes@gmail.com>
2020-07-07 21:32:18 -0400

    completed contract

--- app/src/main/java/com/nycapp/nycaid/Presenter/Contract.java
+++ app/src/main/java/com/nycapp/nycaid/Presenter/Contract.java
@@ -1,4 +1,22 @@
 package com.nycapp.nycaid.Presenter;
 
 public interface Contract {
+
+    interface gngListView {
+        void showGnGSites();
+        void showError();
+    }
+
+    interface testingListView{
+        void showTestingSites();
+        void showError();
+    }
+
+    interface gngPresenter{
+        void getGnGSitesCall();
+    }
+
+    interface testingPresenter{
+        void getTestingSitesCall();
+    }
 }
