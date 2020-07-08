e92e68fd5cda7b0bfb53ed760b70f22d8760dd39
Jperzval <perezvalle.jes@gmail.com>
2020-07-07 22:00:04 -0400

    created FoodGrab model class

--- app/src/main/java/com/nycapp/nycaid/Network/NycAidAPI.java
+++ app/src/main/java/com/nycapp/nycaid/Network/NycAidAPI.java
@@ -1,4 +1,13 @@
 package com.nycapp.nycaid.Network;
 
+import android.database.Observable;
+
+import retrofit2.http.GET;
+
 public interface NycAidAPI {
+
+//    @GET("grabandgolocations?token=AKSENFFO4CJ4OQK7K4QMRUC7AUSRE")
+//    Observable<>
+
 }
+
