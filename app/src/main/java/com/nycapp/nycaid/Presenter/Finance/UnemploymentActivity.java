63ef94357e7dd11042848427b34e8adbeef98b23
Jperzval <perezvalle.jes@gmail.com>
2020-07-09 20:26:48 -0400

    added home icon to actionbar in unemploymentactivity

--- app/src/main/java/com/nycapp/nycaid/Presenter/Finance/UnemploymentActivity.java
+++ app/src/main/java/com/nycapp/nycaid/Presenter/Finance/UnemploymentActivity.java
@@ -2,8 +2,12 @@ package com.nycapp.nycaid.Presenter.Finance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.nycapp.nycaid.Presenter.HomeActivity;
 import com.nycapp.nycaid.R;
 
 public class UnemploymentActivity extends AppCompatActivity {
@@ -12,5 +16,21 @@ public class UnemploymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unemployment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.home) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            return (true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
